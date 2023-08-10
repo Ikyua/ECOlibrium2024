c8b232fda801bcfecd2afb901b2d72863612510f
Isaacamar <113270099+Isaacamar@users.noreply.github.com>
2023-08-10 13:14:29 -0400

    Update kstobe_pms5003.ino

--- kstobe_pms5003.ino
+++ kstobe_pms5003.ino
@@ -1,8 +1,6 @@
 #include <Arduino.h>
-
 #include "PMS.h"
 
- 
 
 #define PMS_SET_PIN 26
 #define PMS_RST_PIN 25
@@ -14,10 +12,6 @@ uint8_t pms_tick_count = PMS_READ_INTERVAL;
 PMS pms(Serial2);
 PMS::DATA data;
 
- 
-
-/***************************************************/
-
 static void setup_pins(void) {
   pinMode(PMS_RST_PIN, OUTPUT);
   digitalWrite(PMS_RST_PIN, HIGH);
@@ -25,146 +19,63 @@ static void setup_pins(void) {
   digitalWrite(PMS_SET_PIN, HIGH);
 }
 
- 
-
-/***************************************************/
-
 static void toggle_set(bool sleep) {
-
   if (sleep) {
-
     digitalWrite(PMS_SET_PIN, LOW);
-
   } else {
-
     digitalWrite(PMS_SET_PIN, HIGH);
-
   }
-
-  delay(500U);
-
+  delay(500);
 }
-
- 
-
-/***************************************************/
 
 static void toggle_reset(void) {
-
   digitalWrite(PMS_RST_PIN, LOW);
-
-  delay(500U);
-
+  delay(500);
   digitalWrite(PMS_RST_PIN, HIGH);
-
-  delay(500U);
-
+  delay(500);
 }
-
- 
-
-/***************************************************/
 
 bool pms5003_init(void) {
-
   setup_pins();
-
-  Serial2.begin(9600, SERIAL_8N1, 16U, 17U);
-
-  delay(1000U);
-
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
+  delay(1000);
   pms_tick_count = PMS_READ_INTERVAL;
-
   return true;
-
 }
 
- 
-
-/***************************************************/
-
-bool pms5003_read(uint16_t *pmSp1_0, uint16_t *pmSp2_5, uint16_t *pmSp10_0,
-
-                  uint16_t *pmAe1_0, uint16_t *pmAe2_5, uint16_t *pmAe10_0) {
-
+bool pms5003_read(uint16_t *pmSp1_0, uint16_t *pmSp2_5, uint16_t *pmSp10_0, uint16_t *pmAe1_0, uint16_t *pmAe2_5, uint16_t *pmAe10_0) {
   bool result = false;
-
- 
-
-  if ((NULL == pmSp1_0) || (NULL == pmSp2_5) || (NULL == pmSp10_0)
-
-  || (NULL == pmAe1_0) || (NULL == pmAe2_5) || (NULL == pmAe10_0)) {
-
+  if ((NULL == pmSp1_0) || (NULL == pmSp2_5) || (NULL == pmSp10_0) || (NULL == pmAe1_0) || (NULL == pmAe2_5) || (NULL == pmAe10_0)) {
     result = false;
-
-  }
-
-  else {
-
+  } else {
     pms_tick_count++;
-
     if (pms_tick_count == PMS_READ_DELAY) {
-
-      PMS::DATA data;
-
-      while (Serial2.available())
-
-      {
-
-        Serial2.read();
-
+      while (Serial1.available()){      
+        Serial1.read();
       }
-
- 
-
-      if (pms.readUntil(data, 2U*PMS::SINGLE_RESPONSE_TIME))
-
-      {
-
+      if (pms.readUntil(data, 2*PMS::SINGLE_RESPONSE_TIME)){
         *pmSp1_0 = data.PM_AE_UG_1_0;
-
         *pmSp2_5 = data.PM_AE_UG_2_5;
-
         *pmSp10_0 = data.PM_AE_UG_10_0;
-
         *pmAe1_0 = data.PM_SP_UG_1_0;
-
         *pmAe2_5 = data.PM_SP_UG_2_5;
-
-        *pmAe1_0 = data.PM_SP_UG_10_0;
-
+        *pmAe1_0 = data.PM_SP_UG_10_0;\
         result = true;
-
-      }
-
-      else
-
-      {
-
+      } else {
         toggle_reset();
-
       }
-
       toggle_set(true);
-
-    } else if (pms_tick_count >= PMS_READ_INTERVAL) {
-
+      } else if (pms_tick_count >= PMS_READ_INTERVAL) {
       toggle_set(false);
-
-      pms_tick_count = 0U;
-
+      pms_tick_count = 0;
     }
-
   }
-
- 
-
   return result;
-
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial2.begin(9600,SERIAL_8N1, 16, 17);
   Serial.print("HELLO");
   delay(1000);
 }
@@ -178,12 +89,12 @@ void loop() {
   uint16_t pmAe10_0 = 0;
 
   if (pms5003_read(&pmSp1_0, &pmSp2_5, &pmSp10_0, &pmAe1_0, &pmAe2_5, &pmAe10_0)) {
-    Serial.print(String(pmSp1_0));
-    Serial.print(String(pmSp2_5));
-    Serial.print(String(pmSp10_0));
-    Serial.print(String(pmAe1_0));
-    Serial.print(String(pmAe2_5));
-    Serial.print(String(pmAe10_0));
+    Serial.println(String(pmSp1_0));
+    Serial.println(String(pmSp2_5));
+    Serial.println(String(pmSp10_0));
+    Serial.println(String(pmAe1_0));
+    Serial.println(String(pmAe2_5));
+    Serial.println(String(pmAe10_0));
   }
 
   delay(60000);
