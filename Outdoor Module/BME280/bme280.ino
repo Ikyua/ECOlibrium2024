7839bd981f21a804e1036f810f2a565095c7a1ba
Manhimliu <132189892+Manhimliu@users.noreply.github.com>
2023-08-15 17:48:49 -0400

    Add files via upload

--- Outdoor Module/BME280/bme280.ino
+++ Outdoor Module/BME280/bme280.ino
@@ -2,17 +2,9 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
 unsigned long delayTime;
 
@@ -22,8 +14,6 @@ void setup() {
 
   bool status;
 
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
   status = bme.begin(0x76);  
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
@@ -36,7 +26,6 @@ void setup() {
   Serial.println();
 }
 
-
 void loop() { 
   printValues();
   delay(delayTime);
@@ -46,11 +35,6 @@ void printValues() {
   Serial.print("Temperature = ");
   Serial.print(bme.readTemperature());
   Serial.println(" *C");
-  
-  // Convert temperature to Fahrenheit
-  /*Serial.print("Temperature = ");
-  Serial.print(1.8 * bme.readTemperature() + 32);
-  Serial.println(" *F");*/
   
   Serial.print("Pressure = ");
   Serial.print(bme.readPressure() / 100.0F);
