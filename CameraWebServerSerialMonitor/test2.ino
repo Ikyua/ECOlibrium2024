4f91160299bf821902eeca5dfb7afdfdda9843b7
Isaacamar <113270099+Isaacamar@users.noreply.github.com>
2023-08-16 15:01:54 -0400

    Update test2.ino

--- CameraWebServerSerialMonitor/test2.ino
+++ CameraWebServerSerialMonitor/test2.ino
@@ -1,93 +1,40 @@
-// ... [Starting code as before]
+#include <SD_MMC.h>
+#include <esp_camera.h>
+#include <WiFiManager.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
-void setup() {
-  Serial.begin(115200);
-  startMicroSD();
-  pinMode(FLASH_PIN, OUTPUT);
-  digitalWrite(FLASH_PIN, LOW);
-  WiFi.mode(WIFI_AP_STA);
-  Serial.setDebugOutput(true);
-  Serial.println();
-  
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_VGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-    config.fb_location = CAMERA_FB_IN_DRAM;
-  }
+const char *soft_ap_ssid = "ESP32-CAM";
+const char *soft_ap_password = "testpassword";
+#define MINUTES_BETWEEN_PHOTOS 1
+#define FLASH_PIN 4
 
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    printAndBuffer("Camera init failed with error 0x" + String(err, HEX));
-    return;
-  }
+String serialBuffer = "";
+AsyncWebServer server(82);
 
-  sensor_t * s = esp_camera_sensor_get();
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  if(psramFound()){
-    s->set_framesize(s, FRAMESIZE_UXGA);
-  } else {
-    s->set_framesize(s, FRAMESIZE_VGA);
-  }
+#define RXD0 3  // GPIO3, IO3
+#define TXD0 1  // GPIO1, IO1
+TinyGPSPlus gps;
+HardwareSerial GPSSerial(1);
 
-  WiFiManager wm;
-  wm.setConfigPortalBlocking(true);
-  wm.setConfigPortalTimeout(120);
-  if(wm.autoConnect("ESP32-CAMAP")){
-        printAndBuffer("Sensor Connected :)");
-  } else {
-        printAndBuffer("AccessPoint Only Mode");
-  }
-  WiFi.softAP(soft_ap_ssid, soft_ap_password);
-
-  startCameraServer();
-
-  if (WiFi.status() == WL_CONNECTED) {
-    printAndBuffer("Camera Ready! Use http://");
-    printAndBuffer(WiFi.localIP().toString());
-  }
-  printAndBuffer("SoftAP IP: http://");
-  printAndBuffer(WiFi.softAPIP().toString());
-
-  server.on("/server", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-  server.on("/server/serial", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", getBufferContents().c_str());
-  });
-  server.begin();
+void printAndBuffer(String message, bool newline = true) {
+    // ... [Same as before]
 }
 
-// ... [Rest of the code as before]
+bool startMicroSD() {
+    // ... [Same as before]
+}
+
+void takePhoto(String filename) {
+    // ... [Same as before]
+}
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+void startCameraServer();
 
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
@@ -96,10 +43,10 @@ const char index_html[] PROGMEM = R"rawliteral(
   <script src="https://kit.fontawesome.com/d91e44f906.js" crossorigin="anonymous"></script>
   <style>
     html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
+      font-family: Arial;
+      display: inline-block;
+      margin: 0px auto;
+      text-align: center;
     }
     h2 { font-size: 3.0rem; }
     p { font-size: 3.0rem; }
@@ -120,6 +67,11 @@ const char index_html[] PROGMEM = R"rawliteral(
 <div style="height:300px; overflow:auto; border:1px solid #ccc; padding:5px;">
   <pre id="serialOutput">%SERIALOUTPUT%</pre>
 </div>
+<p>
+  <i class="fas fa-location-arrow" style="color:#0099ff;"></i>
+  <span class="dht-labels">GPS Location:</span>
+</p>
+<div id="gpsOutput">Fetching...</div>
 </body>
 <script>
 setInterval(function ( ) {
@@ -132,5 +84,60 @@ setInterval(function ( ) {
   xhttp.open("GET", "/serial", true);
   xhttp.send();
 }, 5000 );
+
+function fetchGPSData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("gpsOutput").innerHTML = this.responseText;
+    }
+    if (this.readyState == 4 && this.status == 500) {
+      document.getElementById("gpsOutput").innerHTML = "Error fetching GPS data.";
+    }
+  };
+  xhttp.open("GET", "/gps", true);
+  xhttp.send();
+}
+fetchGPSData();
+setInterval(fetchGPSData, 10000);
+
 </script>
 </html>)rawliteral";
+
+String processor(const String &var) {
+    // ... [Same as before]
+}
+
+void setup() {
+    // ... [Your previous setup code]
+
+    // Initialize the GPS
+    GPSSerial.begin(9600, SERIAL_8N1, RXD0, TXD0);
+
+    // ... [Rest of your setup code]
+}
+
+void loop() {
+    // ... [Your previous loop code]
+
+    // Read data from GPS and update the GPS object
+    while (GPSSerial.available()) {
+        gps.encode(GPSSerial.read());
+    }
+
+    // ... [Rest of your loop code]
+}
+
+// ... [Your existing server routes]
+
+server.on("/gps", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String gpsData;
+    if (gps.location.isValid()) {
+        gpsData = "Latitude: " + String(gps.location.lat(), 6) + ", Longitude: " + String(gps.location.lng(), 6);
+        request->send(200, "text/plain", gpsData);
+    } else {
+        request->send(500, "text/plain", "Error: GPS not fixed yet.");
+    }
+});
+
+// ... [Your main function]
