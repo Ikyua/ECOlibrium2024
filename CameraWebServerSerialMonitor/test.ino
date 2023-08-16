f7aab222b93168f0742070d91414466ecd8c78cf
Isaacamar <113270099+Isaacamar@users.noreply.github.com>
2023-08-16 16:29:06 -0400

    Update test.ino

--- CameraWebServerSerialMonitor/test.ino
+++ CameraWebServerSerialMonitor/test.ino
@@ -1,9 +1,9 @@
-//Must use esp32 v2.0.3rc-1
 #include <SD_MMC.h>
 #include "esp_camera.h"
 #include <WiFiManager.h>
 #include <AsyncTCP.h>
 #include "ESPAsyncWebServer.h"
+#include <TinyGPS++.h>
 
 AsyncWebServer server(82);
 
@@ -154,74 +154,10 @@ void setup() {
   Serial.setDebugOutput(true);
   Serial.println();
   
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_VGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-    config.fb_location = CAMERA_FB_IN_DRAM;
-  }
-
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    printAndBuffer("Camera init failed with error 0x" + String(err, HEX));
-    return;
-  }
-
-  sensor_t * s = esp_camera_sensor_get();
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  if(psramFound()){
-    s->set_framesize(s, FRAMESIZE_UXGA);
-  } else {
-    s->set_framesize(s, FRAMESIZE_VGA);
-  }
-
-  WiFiManager wm;
-  wm.setConfigPortalBlocking(true);
-  wm.setConfigPortalTimeout(120);
-  if(wm.autoConnect("ESP32-CAMAP")){
-        printAndBuffer("Sensor Connected :)");
-  } else {
-        printAndBuffer("AccessPoint Only Mode");
-  }
-  WiFi.softAP(soft_ap_ssid, soft_ap_password);
-
-  startCameraServer();
-
-  if (WiFi.status() == WL_CONNECTED) {
-    printAndBuffer("Camera Ready! Use http://");
-    printAndBuffer(WiFi.localIP().toString());
-  }
-  printAndBuffer("SoftAP IP: http://");
-  printAndBuffer(WiFi.softAPIP().toString());
+  // Initialize GPS UART
+  GPSSerial.begin(9600, SERIAL_8N1, 1, 3); // UART0, TX on GPIO1, RX on GPIO3
+  
+  // ... Rest of the setup code ...
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, processor);
@@ -246,5 +182,9 @@ void loop() {
     filename += number;
     filename += ".jpg";
     takePhoto(filename);
+  }
+
+  while (GPSSerial.available()) {
+    gps.encode(GPSSerial.read());
   }
 }
